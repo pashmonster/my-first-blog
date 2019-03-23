d8002291a7b5888dd74cc66d0812ce8a66d6e021
Ava <ava@oqtol.com>
2019-03-23 17:19:01 -0400

    My Django Girls app, first commit

---
+++ guess_my_number.py
@@ -0,0 +1,33 @@
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Ava")
+
+
+
+
+
+
+
+"""
+def say_my_name(name):
+    print("hello there",name)
+
+say_my_name("brenna")
+"""
+
+
+
+"""
+def hi():
+
+    print('how are you')
+
+    print('how are you today')
+    return('sup')
+
+
+print(hi())
+"""
---
+++ python_intro.py
@@ -0,0 +1,64 @@
+"""
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! :(")
+"""
+print('Hello, Django girls!')
+
+"""
+#if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Ava':
+    print('Hey Ava!')
+else:
+    print('Hey anonymous!')
+"""
+
+#if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
